b781d0d60215e05c8f8f56a2f5f2ba750fc5b885
hassan Mattar <hassan.mattar.id@gmail.com>
2020-08-19 15:13:27 +0300

    first commit

---
+++ Dynamic Button/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Dynamic_Button
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Price";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Service Name";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnadd
+            // 
+            this.btnadd.BackgroundImage = global::Dynamic_Button.Properties.Resources.iconfinder_icon_ios7_plus_211803__1_;
+            this.btnadd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnadd.FlatAppearance.BorderSize = 0;
+            this.btnadd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnadd.Location = new System.Drawing.Point(12, 71);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(39, 20);
+            this.btnadd.TabIndex = 3;
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(796, 473);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnadd;
+    }
+}
+
---
+++ Dynamic Button/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Dynamic_Button
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadForm();
+        }
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=test;Integrated Security=True");
+        int count = 1;
+        int x = 0;
+        int y = 0;
+        int btnLocation = 97;
+        int txtLocation = 100;
+        void LoadForm()
+        {
+            con.Open();
+            SqlCommand select = new SqlCommand("select name,price from service",con);
+            SqlDataReader da = select.ExecuteReader();
+            while(da.Read())
+            {
+                for (int i = 0; i < 1; i++)
+                {
+
+                    Button btn = new Button();
+                    btn.Text = da.GetValue(0).ToString();
+                    btn.Name = count.ToString();
+                    btn.Size = new Size(100, 23);
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.BorderSize = 0;
+                    btn.BackColor = Color.GhostWhite;
+                    btn.Location = new Point(57, btnLocation);
+                    TextBox txt = new TextBox();
+                    txt.Text = da.GetValue(1).ToString();
+                    txt.TextAlign = HorizontalAlignment.Center;
+                    txt.Name = count.ToString();
+                    txt.Size = new Size(100, 20);
+                    txt.Location = new Point(163, txtLocation);
+                    y++;
+                    count++;
+                    Controls.Add(btn);
+                    Controls.Add(txt);
+                    btnLocation = btnLocation + 29;
+                    txtLocation = txtLocation + 29;
+                }
+            }
+        }
+        private void btnadd_Click_1(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 1; i++)
+            {
+
+                Button btn = new Button();
+                btn.Text = "1 MONTH MTC";
+                btn.Name = count.ToString();
+                btn.Size = new Size(100, 23);
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 0;
+                btn.BackColor = Color.GhostWhite;
+                btn.Location = new Point(57, btnLocation);
+                TextBox txt = new TextBox();
+                txt.Text = "15000";
+                txt.TextAlign = HorizontalAlignment.Center;
+                txt.Name = count.ToString();
+                txt.Size = new Size(100, 20);
+                txt.Location = new Point(163, txtLocation);
+                y++;
+                count++;
+                Controls.Add(btn);
+                Controls.Add(txt);
+                btnLocation = btnLocation + 29;
+                txtLocation = txtLocation + 29;
+            }
+
+        }
+    }
+}
